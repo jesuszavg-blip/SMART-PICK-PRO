9761edfd9c7baf864f1cdee67b7df4ee1cfc2f13
jesuszavg-blip <jesuszavg@gmail.com>
2026-09-01 14:28:16 -0700

    Actualizacion automatica: config.py

--- config.py
+++ config.py
@@ -18,11 +18,14 @@ ADMIN_INIT_PASS = os.getenv("ADMIN_INIT_PASS", "SmartVIP2026!")
 # Enlaces de Casas de Apuestas (Afiliados / Referencia)
 ENLACES_CASINOS = {
     "1xBet": "https://reffpa.com/L?tag=d_6029550m_1599c_&site=6029550&ad=1599",
+    "Mexplay": "https://mexplay.mx/?referral=mqx6lb",
     "Caliente": "https://www.caliente.mx/ofertas/raf/?member=CALIRAF&var1=undefined",
     "Betmaster": "https://betmaster.net/?rsd=UmRoJ7BQ",
     "Winpot": "https://winpot.mx/r/5296706_0Socp8oK",
 }
 
+ENLACE_1XBET = ENLACES_CASINOS["1xBet"]
+ENLACE_MEXPLAY = ENLACES_CASINOS["Mexplay"]
 ENLACE_POR_DEFECTO = ENLACES_CASINOS["1xBet"]
 ENLACE_WHATSAPP = "https://wa.me/526676947014?text=Hola%20Jesus,%20quiero%20obtener%20acceso%20a%20Smart%20Pick%20Pro"
 # Datos de Pago Directo (BanCoppel y PayPal)
